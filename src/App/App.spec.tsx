7258100d463a7cb462b14fa5a83d99f1ea5ceb34
ben liu <beliu@seek.com.au>
2025-11-05 12:04:27 +1100

    doc: doco

--- src/App/App.spec.tsx
+++ src/App/App.spec.tsx
@@ -8,11 +8,13 @@ import { App } from './App';
 describe('App', () => {
   it('renders the hidden component with text', () => {
     render(
+      // Mock the browser width to be `desktop`
       <BraidTestProvider breakpoint="desktop">
         <App />
       </BraidTestProvider>,
     );
 
+    // Given `Hidden below="tablet"`, `desktop` width should be visible
     expect(screen.getByText('Hello world (greater than tablet)')).toBeVisible();
   });
 });
