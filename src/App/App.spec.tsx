9dc0293978eb7ac76343dd535b9a7b689cb97ca1
ben liu <beliu@seek.com.au>
2025-11-05 11:57:16 +1100

    test: setup tests

--- src/App/App.spec.tsx
+++ src/App/App.spec.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import { BraidTestProvider } from 'braid-design-system/test';
 
+import '@testing-library/jest-dom';
+
 import { App } from './App';
 
 describe('App', () => {
